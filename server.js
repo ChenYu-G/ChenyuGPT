5e3d2acad2be08fce05f515fc827ebd8152c7db0
ChenYu <cgeng.charlie@gmail.com>
2023-05-02 00:48:28 -0500

    adding .env

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express")
-const cors = require("cors")
+const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const PORT = 8000
@@ -7,25 +7,25 @@ const PORT = 8000
 app.use(express.json())
 app.use(cors())
 
-const API_KEY = "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"
+const API_KEY = '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'
 
-app.post("/completions", async (req, res) => {
+app.post('/completions', async (req, res) => {
 	const options = {
-		method: "POST",
+		method: 'POST',
 		headers: {
 			Authorization: `Bearer ${API_KEY}`,
-			"Content-Type": "application/json",
+			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify({
-			model: "gpt-3.5-turbo",
-			messages: [{ role: "user", content: req.body.message }],
+			model: 'gpt-3.5-turbo',
+			messages: [{ role: 'user', content: req.body.message }],
 			temperature: 0.7,
 			max_tokens: 10,
 		}),
 	}
 	try {
 		const response = await fetch(
-			"https://api.openai.com/v1/chat/completions",
+			'https://api.openai.com/v1/chat/completions',
 			options
 		)
 		const data = await response.json()
